0f08a2f4deba1d272b4d79146594e3c94cf6eb12
Josh Everett <josh@bandpage.com>
2013-09-30 00:05:46 -0700

    Unit tests.

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,25 @@
-test('hello test', function () {
-  ok(1 == '1', 'Passed!');
+_rekwire.base = '/';
+
+rekwire('addthis', '//s7.addthis.com/js/250/addthis_widget.js#domready=1&async=1');
+
+rekwire('derp');
+rekwire('foo');
+
+asyncTest('Loading same origin scripts.', function () {
+  expect(2);
+
+  $.when(rekwire('derp'), rekwire('foo')).done(function () {
+    equal(derp, 'derp', 'Module "derp" loaded.');
+    equal(foo, 'foo', 'Module "foo" loaded.');
+    start();
+  });
+});
+
+asyncTest('Loading external script.', function () {
+  expect(1);
+
+  rekwire('addthis').done(function () {
+    ok(!!addthis, 'External module "addthis" loaded.');
+    start();
+  });
 });
